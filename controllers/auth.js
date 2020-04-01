98f0f2141560ff68991b9f06ea72251ddbbea256
Moustafa <bendib.moustafa@gmail.com>
2020-04-01 22:52:13 +0100

    clean up the code

--- controllers/auth.js
+++ controllers/auth.js
@@ -229,41 +229,6 @@ exports.confirmUser = asyncHandler(async(req, res, next) => {
 // @access   Private
 exports.addImage = asyncHandler(async(req,res,next) => {
 
-
-    // const user = await User.findById(req.user.id);
-
-    // if (!user) {
-    //     return next(new ErrorResponse('Not allowed to do the operation', 401))
-    // }
-
-    // const file = req.files.file
-
-    // if(!file.mimetype.startsWith('image')) {
-    //     return next(new ErrorResponse('File type not suported', 400));
-    // }
-    
-    
-    // if(file.size > 100000) {
-    //     return next(new ErrorResponse('File size ecceeds limit', 400))
-    // }
-
-    // file.name = `Photo_${user.id}${path.parse(file.name).ext}`
-
-
-    // file.mv(`${process.env.STATIC_PATH}/${file.name}`, async err => {
-    //     if (err) {
-    //         console.log(err)
-    //         return next(new ErrorResponse('Server Error', 500))
-    //     }
-
-    //     await User.findByIdAndUpdate(req.user.id, {image: file.name})
-
-    //     res.status(200).json({
-    //         success: true,
-    //         data: file.name
-    //     })
-    // }) 
-
     res.status(200).json(res.uploadImage);
 })
 
