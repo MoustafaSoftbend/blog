8e469d3f147c3e79b6c79a896a88ba99936694db
mosben <bendib.moustafa@gmail.com>
2024-09-21 15:40:06 +0100

    fixed navbar problem

--- src/src/components/layout/Navbar.js
+++ src/src/components/layout/Navbar.js
@@ -1,37 +1,37 @@
-import React, {NavLink, Fragment, useEffect, PropTypes} from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { NavLink } from 'react-router-dom'; // Correctly import NavLink from react-router-dom
+import PropTypes from 'prop-types';
 
-const Navbar = ({tab}) => {
+const Navbar = ({ tab }) => {
+  return (
+    <Fragment>
+      <nav id="navbar">
+        <div className="nav-container">
+          <a href="/" className="nav-logo">Viral Tech</a>
+          <a href="#" className="hamburger-menu">
+            <i className="fa fa-bars fa-2x"></i>
+          </a>
 
-    return(
-        <Fragment>
-          <nav id="navbar">
-            <div className="nav-container">
-              <a href="/" className="nav-logo">Viral Tech</a>
-              <a href="#" className="hamburger-menu"
-                ><i className="fa fa-bars fa-2x"></i
-              ></a>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to="/" className={`nav-link ${tab === 'index' ? 'active' : ''}`}>Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/post" className={`nav-link ${tab === 'post' ? 'active' : ''}`}>Blog</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className={`nav-link ${tab === 'about' ? 'active' : ''}`}>About</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Fragment>
+  );
+};
 
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a href="/" className={`nav-link ${tab=='index'? 'active': ''}`}>Home</a>
-                </li>
-                <li className="nav-item">
-                  <a href="/post" className={`nav-link ${tab=='post' ? 'active': ''}`}>Blog</a>
-                </li>
-                <li className="nav-item">
-                  <a href="/about" className={`nav-link ${tab=='about'? 'active': ''}`}>About</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </Fragment>
-
-    )
-    Navbar.propTypes = {
+// Define prop types outside the component
+Navbar.propTypes = {
   tab: PropTypes.string.isRequired
-  }
-
-}
-
+};
 
 export default Navbar;
